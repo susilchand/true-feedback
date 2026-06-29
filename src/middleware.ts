b086ecd4d3149064f407be25ac37e1104703499b
Susil Chand <susilkyan1@gmail.com>
2026-06-29 23:12:50 +0630

    Add FrontEnd

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getToken } from "next-auth/jwt";
 export { default } from 'next-auth/middleware'
+
 export async function middleware(request: NextRequest) {
 
   const token = await getToken({ req: request })
@@ -18,7 +19,12 @@ export async function middleware(request: NextRequest) {
 
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
-  // return NextResponse.redirect(new URL('/home', request.url))
+  if (!token && url.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/sign-in', request.url))
+
+  }
+
+  return NextResponse.next()
 
 }
 
